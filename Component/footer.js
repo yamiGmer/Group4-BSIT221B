8d836e63ecbf08a5657f2620aa12e289d52f244a
yamiGmer <148083837+yamiGmer@users.noreply.github.com>
2024-02-29 16:59:18 +0800

    added to nextjsapp

---
+++ Component/footer.js
@@ -0,0 +1,8 @@
+function Footer(){
+    return(
+        <footer>
+        <p>&copy; 2024 Taste Of Japan. All rights reserved.</p>
+    </footer>
+    )
+}
+export default Footer
---
+++ Component/header.js
@@ -0,0 +1,20 @@
+
+
+
+
+function Header(){
+    return(
+        <header>
+        <h1>Taste Of Japan</h1>
+            <nav>
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#mains">Mains</a></li>
+                    <li><a href="#noodles">Noodles</a></li>
+                    <li><a href="#sides">Sides</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+export default Header
