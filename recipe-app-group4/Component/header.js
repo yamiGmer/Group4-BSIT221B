6c80351fa7a73f54205932da6c15f07e6cb44d7f
yamiGmer <148083837+yamiGmer@users.noreply.github.com>
2024-02-29 22:59:32 +0800

    adjusted imports of css

--- recipe-app-group4/Component/header.js
+++ recipe-app-group4/Component/header.js
@@ -1,20 +1,22 @@
-
+import styles from '@/styles/header.module.css'
 
 
 
 function Header(){
     return(
-        <header>
-        <h1>Taste Of Japan</h1>
-            <nav>
-                <ul>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#mains">Mains</a></li>
-                    <li><a href="#noodles">Noodles</a></li>
-                    <li><a href="#sides">Sides</a></li>
+        <main className={styles.main}>
+        <header className={styles.header}>
+        <h1 className={styles.h1}>Taste Of Japan</h1>
+            <nav className={styles.nav}>
+                <ul className={styles.nav}>
+                    <li className={styles.li}><a href="#home" className={styles.a}>Home</a></li>
+                    <li className={styles.li}><a href="#mains" className={styles.a}>Mains</a></li>
+                    <li className={styles.li}><a href="#noodles" className={styles.a}>Noodles</a></li>
+                    <li className={styles.li}><a href="#sides" className={styles.a}>Sides</a></li>
                 </ul>
             </nav>
         </header>
+        </main>
     )
 }
 export default Header
