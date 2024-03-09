e24d9773d0de336232d2be3ad0c5040d5a85bdf1
yamiGmer <148083837+yamiGmer@users.noreply.github.com>
2024-03-10 05:09:47 +0800

    Modified header to use grids instead of list

--- recipe-app-group4/Component/pagesHeader.js
+++ recipe-app-group4/Component/pagesHeader.js
@@ -1,26 +1,37 @@
 import styles from '@/styles/header.module.css'
 import Link from "next/link";
-import { Header, Segment } from 'semantic-ui-react'
+import { Header } from 'semantic-ui-react'
 import { ListItem, ListContent, List} from 'semantic-ui-react'
-import { GridRow, GridColumn, Grid } from 'semantic-ui-react'
+import { GridRow, GridColumn, Grid, Segment } from 'semantic-ui-react'
 
 
 
 function PagesHeader(){
     return(
         <Header className={styles.header} textAlign='center'>
-            <Grid>
-                <GridRow>
+            <Grid verticalAlign={'bottom'}>
+                <GridRow columns={1}>
                     <GridColumn>
-                        <Header size='huge'>Taste of Japan</Header>
+                            <Header size='huge' className=''>Taste of Japan</Header>
                     </GridColumn>
-                </GridRow>
-            </Grid>               
-            <Grid>
-                <GridRow>
-                    <GridColumn>
-                        <CategoryList/>
-                    </GridColumn>
+                </GridRow>    
+
+                <GridRow/><GridRow/>          
+                <GridRow columns={4} verticalAlign='bottom'>
+                        <GridColumn>
+                            <Link href={{pathname: '/'}}>Home</Link>
+                        </GridColumn>
+                        <GridColumn>
+                            <Link href={{pathname: '/recipeCategory/Main'}}>Main</Link>
+                        </GridColumn>
+                  
+                        <GridColumn>
+                            <Link href={{pathname: '/recipeCategory/Sides'}}>Sides</Link>
+                        </GridColumn>
+                        <GridColumn>
+                            <Link href={{pathname: '/recipeCategory/Noodles'}}>Noodles</Link>
+                        </GridColumn>
+                   
                 </GridRow>
             </Grid> 
 
@@ -29,25 +40,3 @@ function PagesHeader(){
     )
 }
 export default PagesHeader
-
-function CategoryList(){
-    return(
-        <List horizontal celled relaxed size='massive' className={styles.lists}>
-                    <ListItem>
-                        <ListContent >
-                            <Link href={{pathname: '/'}}>Home</Link>
-                        </ListContent>
-                        
-                    </ListItem>
-                    <ListItem >
-                        <Link href={{pathname: '/recipeCategory/Main'}}>Main</Link>
-                    </ListItem>
-                    <ListItem >
-                        <Link href={{pathname: '/recipeCategory/Sides'}}>Sides</Link>
-                    </ListItem>
-                    <ListItem >
-                        <Link href={{pathname: '/recipeCategory/Noodles'}}>Noodles</Link>
-                    </ListItem>
-                </List>
-    )
-}
