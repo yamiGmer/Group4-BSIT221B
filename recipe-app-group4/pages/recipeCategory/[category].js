20b32d14d3a790fdc5d9c06671013e101acc82d9
Asianhavoc <miguel.orias02@gmail.com>
2024-03-10 21:57:38 +0800

    margin and font for "back" button

--- recipe-app-group4/pages/recipeCategory/[category].js
+++ recipe-app-group4/pages/recipeCategory/[category].js
@@ -8,10 +8,9 @@ import path from 'path';
 import styles from '@/styles/category.module.css';
 import { Grid } from 'semantic-ui-react';
 
-function RecipeCategory ({ category }) {
+function RecipeCategory({ category }) {
     const router = useRouter();
     const foodCategory = router.query.category;
-    console.log(foodCategory);
 
     return (
         <>
@@ -20,21 +19,21 @@ function RecipeCategory ({ category }) {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
             </Head>
             <Header />
-            <h1>Testing<br/>Food Category: {foodCategory}</h1>  
+            <h1>Testing<br />Food Category: {foodCategory}</h1>
             <Grid relaxed columns={3}>
                 {category.map(item => {
                     if (foodCategory === item.classification.category) {
                         return (
                             <Grid.Column key={item.num}>
                                 <Link href={{ pathname: '/recipePage/' + item.foodName.toString() }}>
-                                    <section className={styles.category}> {/* Apply category className here */}
+                                    <section className={styles.category}>
                                         <p>{"Food: " + item.foodName}</p>
                                     </section>
-
+                                    
                                     <section>
                                     <img src={`/./././${item.image}`} className={styles.image} alt={item.foodName} />
                                     </section>
-
+                                    
                                 </Link>
                             </Grid.Column>
                         )
@@ -42,8 +41,8 @@ function RecipeCategory ({ category }) {
                 })}
             </Grid>
             <Link href={{ pathname: '../' }}>
-                <section className={styles.category}>
-                <h1>Back</h1>
+                <section className={styles.back}>
+                    <h1>Back</h1>
                 </section>
             </Link>
             <Footer />
@@ -68,11 +67,11 @@ export async function getStaticPaths() {
     };
 }
 
-export async function getStaticProps({ params }) {  
+export async function getStaticProps({ params }) {
     const filePath = path.join(process.cwd(), "Component/recipe.json");
     const fileContent = fs.readFileSync(filePath, "utf8");
     const data = JSON.parse(fileContent);
-    
+
     const category = data.filter(recipe => recipe.classification.category === params.category);
 
     return {
