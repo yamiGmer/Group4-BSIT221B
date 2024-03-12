5a3174be21865e053db79acfe91f06317d13d8fa
Carl Ivan Ken Simbulan <161710975+carlivan2525@users.noreply.github.com>
2024-03-12 21:21:06 +0800

    Update footer.js
    footer

--- recipe-app-group4/Component/footer.js
+++ recipe-app-group4/Component/footer.js
@@ -1,12 +1,12 @@
 
-import styles from '@/styles/footer.module.css'
+import styles from '@/styles/footer.module.css';
 
-
-function Footer(){
-    return(
-        <footer className={styles.footer}>
-        <p>&copy; 2024 Taste Of Japan. All rights reserved.</p>
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p style={{ textAlign: 'center', fontSize: '1em' }}>&copy; 2024 Taste Of Japan. All rights reserved.</p>
     </footer>
-    )
+  );
 }
-export default Footer
+
+export default Footer;
