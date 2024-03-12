2dde7b2c4005c66f354be327d1262c740094ed59
yamiGmer <148083837+yamiGmer@users.noreply.github.com>
2024-03-12 16:13:57 +0800

    Changed header to be responsive.

--- recipe-app-group4/Component/pagesHeader.js
+++ recipe-app-group4/Component/pagesHeader.js
@@ -1,36 +1,48 @@
 import styles from '@/styles/header.module.css'
 import Link from "next/link";
 import { Header } from 'semantic-ui-react'
-import { ListItem, ListContent, List} from 'semantic-ui-react'
 import { GridRow, GridColumn, Grid, Segment } from 'semantic-ui-react'
 
-function PagesHeader(){
 
+
+function PagesHeader(){
+    //add keys on grid
     return(
         <Header className={styles.header} textAlign='center'>
-            <Grid verticalAlign={'bottom'}>
-                <GridRow/><GridRow/>
-                <GridRow columns={1}>
-                    <GridColumn>
-                            <Header size='huge' className=''>Taste of Japan</Header>
+            <Grid verticalAlign={'bottom'} key='grid-header'>
+                
+                <GridRow columns={1} key='grid-title-container' className={styles.grid_title_container}>
+                    <GridColumn key='grid-title'>
+                            <Header 
+                            size='huge' 
+                            className={styles.grid_title}
+                            color='blue'
+                            >
+                                <Link 
+                                href={{pathname: '/'}}
+                                className={styles.link}
+                                >
+                                    Taste of Japan
+                                </Link>
+                                
+                            </Header>
                     </GridColumn>
                 </GridRow>    
                
-                <GridRow columns={5}  divided >
-                        <GridColumn>
-                            <Link href={{pathname: '/'}}>Home</Link>
-                        </GridColumn>
-                        <GridColumn>
+                <GridRow columns={4}  divided key='grid-link-container' className={styles.grid_link_container}>
+
+        
+                        <GridColumn key='grid-main'>
                             <Link href={{pathname: '/recipeCategory/Main'}}>Main</Link>
                         </GridColumn>
                   
-                        <GridColumn>
+                        <GridColumn key='grid-sides'>
                             <Link href={{pathname: '/recipeCategory/Sides'}}>Sides</Link>
                         </GridColumn>
-                        <GridColumn>
+                        <GridColumn key='grid-noodles'>
                             <Link href={{pathname: '/recipeCategory/Noodles'}}>Noodles</Link>
                         </GridColumn>
-                        <GridColumn>
+                        <GridColumn key='grid-about'>
                             <Link href={{pathname: '/about'}}>About</Link>
                         </GridColumn>
 
