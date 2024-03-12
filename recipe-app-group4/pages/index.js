004c02115d2d899fceb12ce89cf7f3511cfae526
yamiGmer <148083837+yamiGmer@users.noreply.github.com>
2024-03-12 23:26:30 +0800

    added search function

--- recipe-app-group4/pages/index.js
+++ recipe-app-group4/pages/index.js
@@ -4,42 +4,43 @@ import Link from "next/link";
 import Head from 'next/head';
 import fs from 'fs';
 import path from 'path';
-import styles from '@/styles/home.module.css'
-import React from 'react';
-import 'semantic-ui-css/semantic.min.css'
-import { GridRow, GridColumn, Grid, Image, Header, Segment} from 'semantic-ui-react'
-import { CardMeta, CardHeader, CardDescription, CardContent, Card } from 'semantic-ui-react'
+import styles from '@/styles/home.module.css';
+import React, { useState } from 'react';
+import 'semantic-ui-css/semantic.min.css';
+import { GridRow, GridColumn, Grid, Image, Header, Segment } from 'semantic-ui-react';
+import { CardMeta, CardHeader, CardDescription, CardContent, Card } from 'semantic-ui-react';
 
-export default function Home({recipe}) {
-    return(
-        <>
+export default function Home({ recipe }) {
+  const [searchTerm, setSearchTerm] = useState('');
 
-        <Head>
-          <title>Recipe Page</title>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          
-        </Head>
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
 
-        <PagesHeader/> 
-          
-          <main>
-          <Grid stackable>
-            <GridRow key='title-row' textAlign="center" columns={1}>
-              <GridColumn key='title-column'>
-                <Header as='h1'>Search Bar</Header>
-              </GridColumn>
-            </GridRow>   
-            <GridRow columns={3} relaxed stackable textAlign="center">
-              <GridColumn key='left-column' width={6} textAlign="left">
-                <Segment>
-                  <Header as='h2'>Recommended Pairings</Header>
-                  {
-                  recipe.map(i => {
-                    if(i.recipeId == 4 || i.recipeId == 1){
-                      return(
-                        
-                        <Card key={i.num} textAlign='left' className={styles.card}>
-                        <Image src={`/./././${i.image}`} wrapped ui={false} fluid/>
+  const filteredRecipes = recipe.filter(i => {
+    return i.foodName.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+
+  return (
+    <>
+      <Head>
+        <title>Recipe Page</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
+      <PagesHeader />
+
+      <main>
+        <Grid stackable>
+          <GridRow columns={3} relaxed stackable textAlign="center">
+            <GridColumn key='left-column' width={6} textAlign="left">
+              <Segment>
+                <Header as='h2'>Recommended Pairings</Header>
+                {recipe.map(i => {
+                  if (i.recipeId === 4 || i.recipeId === 1) {
+                    return (
+                      <Card key={i.num} textAlign='left' className={styles.card}>
+                        <Image src={`/./././${i.image}`} wrapped ui={false} fluid />
                         <CardContent>
                           <CardHeader>{i.foodName}</CardHeader>
                           <CardMeta>
@@ -50,23 +51,19 @@ export default function Home({recipe}) {
                           </CardDescription>
                         </CardContent>
                       </Card>
-                      
-                      )
-                    }})
-                    }
-                  
-                </Segment>
-              </GridColumn>
-              <GridColumn key='left-column' width={6} textAlign="left">
-                <Segment>
-                  <Header as='h2'>Recommended by Web Creators</Header>
-                  {
-                  recipe.map(i => {
-                    if(i.recipeId == 5 || i.recipeId == 7){
-                      return(
-                        
-                        <Card key={i.num} textAlign='left' className={styles.card}>
-                        <Image src={`/./././${i.image}`} wrapped ui={false} fluid/>
+                    )
+                  }
+                })}
+              </Segment>
+            </GridColumn>
+            <GridColumn key='middle-column' width={6} textAlign="left">
+              <Segment>
+                <Header as='h2'>Recommended by Web Creators</Header>
+                {recipe.map(i => {
+                  if (i.recipeId === 5 || i.recipeId === 7) {
+                    return (
+                      <Card key={i.num} textAlign='left' className={styles.card}>
+                        <Image src={`/./././${i.image}`} wrapped ui={false} fluid />
                         <CardContent>
                           <CardHeader>{i.foodName}</CardHeader>
                           <CardMeta>
@@ -77,69 +74,74 @@ export default function Home({recipe}) {
                           </CardDescription>
                         </CardContent>
                       </Card>
-                      
-                      )
-                    }})
-                    }
-                  
-                </Segment>
-              </GridColumn>
-              <GridColumn key='right-column' width={3} textAlign="left">
+                    )
+                  }
+                })}
+              </Segment>
+            </GridColumn>
+            <GridColumn key='right-column' width={3} textAlign="left">
               <Segment>
                 <Header as='h2'>Recently Added</Header>
-                {
-
-                  recipe.slice(0).reverse().map(i => {
-                    return(
-
-                      <div key={i.num}>  
-                        <Link href={{pathname: '/recipePage/'+ i.foodName.toString()}}>
-                          <p>{i.recipeId + ". " + i.foodName}</p>
-                        </Link>  
-                        <br></br>
-                      </div>
-                    )
-                  })
-                }
+                {recipe.slice(0).reverse().map(i => (
+                  <div key={i.num}>
+                    <Link href={{ pathname: '/recipePage/' + i.foodName.toString() }}>
+                      <p>{i.recipeId + ". " + i.foodName}</p>
+                    </Link>
+                    <br />
+                  </div>
+                ))}
               </Segment>
-              </GridColumn>  
-            </GridRow>   
-          </Grid>
 
-    
-                  
-         
-          
-         
-          
+              <Segment key='Search-column'>
+              <Header as='h1'>Search Bar</Header>
+              <div className={styles.searchBar}>
+                <input
+                  className={styles.searchInput}
+                  type="text"
+                  placeholder="Looking for something?"
+                  value={searchTerm}
+                  onChange={handleSearchChange}
+                />
+              </div>
+            </Segment>
+            <Segment>
+            <div className="searchResults">
+              {searchTerm && (
+                <Segment>
+                  <Header as='h2'>Search Results</Header>
+                  {filteredRecipes.map(i => (
+                    <div key={i.num}>
+                      <Link href={{ pathname: '/recipePage/' + i.foodName.toString() }}>
+                        <p>{i.recipeId + ". " + i.foodName}</p>
+                      </Link>
+                      <br />
+                    </div>
+                  ))}
+                </Segment>
+              )}
+            </div>
+            </Segment>
 
-          </main>
-      
+            </GridColumn>
+          </GridRow>
+        </Grid>
+
         
-        <Footer/>
+      </main>
 
-      </>
+      <Footer />
+    </>
   );
 }
 
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "Component/recipe.json");
+  const fileContent = fs.readFileSync(filePath, "utf8");
+  const data = JSON.parse(fileContent);
 
-
-
-export async function getStaticProps() {  
-    //LOCAL JSON FETCHING
-  
-      //cwd(current working directory)
-    const filePath = path.join(process.cwd(), "Component/recipe.json");
-    const fileContent = fs.readFileSync(filePath, "utf8")
-      //converted to json object
-    const data = JSON.parse(fileContent);
-    
-  
-    console.log(data);
-    return{
-      props:{
-        recipe: data
-      }
+  return {
+    props: {
+      recipe: data
     }
-  
+  };
 }
